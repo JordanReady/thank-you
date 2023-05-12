eea48b4c7f050c19f5cb96037a379f2e91cf437f
JordanReady <jready112@gmail.com>
2023-05-12 01:42:08 -0500

    thankyou animations

--- src/App.tsx
+++ src/App.tsx
@@ -17,16 +17,29 @@ function App() {
             </div> */}
             <div className="contact-inner-container">
               <div className="thanks-box">
-                <h1 className="contact-title">Thank you!</h1>
+                <h1 className="contact-title">
+                  <span style={{ "--i": "1" }}>T</span>
+                  <span style={{ "--i": "2" }}>h</span>
+                  <span style={{ "--i": "3" }}>a</span>
+                  <span style={{ "--i": "4" }}>n</span>
+                  <span style={{ "--i": "5" }}>k</span>
+                  <span style={{ "--i": "6" }}> </span>
+                  <span style={{ "--i": "7" }}>Y</span>
+                  <span style={{ "--i": "8" }}>o</span>
+                  <span style={{ "--i": "9" }}>u</span>
+                  <span style={{ "--i": "10" }}>!</span>
+                </h1>
                 <p className="contact-subtitle">
                   Your message has been sent! I will get back to you as soon as
                   possible!
                 </p>
-                <img
-                  className="thanks-img img-fluid"
-                  src={HeroImage}
-                  alt="hero"
-                />
+                <div className="hero-img">
+                  <img
+                    className="thanks-img img-fluid"
+                    src={HeroImage}
+                    alt="hero"
+                  />
+                </div>
               </div>
             </div>
           </div>
